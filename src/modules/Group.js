73b86b75946b80b7c905f84e00c998e17503c05c
heyitsbryanm <matiasbryan@gmail.com>
2020-07-29 11:35:23 -0700

    Adds meta information

--- src/modules/Group.js
+++ src/modules/Group.js
@@ -33,7 +33,6 @@ export default class Group extends PureComponent {
         })
     }
 
-
     render() {
         return (
 
--- src/modules/ImageCard.js
+++ src/modules/ImageCard.js
@@ -5,41 +5,44 @@ export default class ImageCard extends PureComponent {
         super(props);
         this.state = {
             url: this.props.image.url,
-            inputText: this.props.image.url
+            inputText: this.props.image.url,
+            editMode: true
         }
+        // inherited props:
+        // this.props.imageOptions
     };
 
-    _renderImage = () =>{
-        if(this.props.imageOptions.parameterSet === false) {
+    _renderImage = () => {
+        if (this.props.imageOptions.parameterSet === false) {
             return this.state.url
         } else if (this.props.imageOptions.parameterSet === true) {
             return this.state.url.split('?')[0].split('#')[0] + '?' + this.props.imageOptions.parameterSetValue
         }
     }
+
     render() {
         return (
             <div class="image-card card">
-                <img alt='placeholder-alt' src={this._renderImage()} />
+                <img alt="placeholder-alt" src={this._renderImage()} />
                 <input type="text" placeholder="Insert image URL here" value={this.state.inputText} onChange={e => {
                     this.setState({ inputText: e.target.value })
                 }}></input>
                 <button type="submit" onClick={e => {
                     this.setState({ url: this.state.inputText })
                 }}>Reload image</button>
+                <textarea disabled={this.state.editMode} className="caption demoElement" value="This is the caption for the image"/>
                 <div className="center subtext metadata">
                     <p>
-                        <span className="bold">url: </span>
-                        <a href='placeholder-link>'>placeholder-link</a>
+                        <a href={this.state.url} target="_blank">Open in new tab</a>
                     </p>
                     <p>
-                        <span className="bold">size: </span>
-                placeholder-size
-              </p>
+                        <a href={`https://sandbox.imgix.com/view?url=${encodeURIComponent(this._renderImage())}`} target="_blank">Open in sandbox</a>
+                    </p>
                     <p className="wrap">
                         <span className="bold">parameters: </span>
-                        <code className="parameters inline">placeholder-parameters</code>
+                        {console.log('this._renderImage() is: ',this._renderImage())}
+                        <code className="parameters inline">{this._renderImage().split('?')[1] || 'No parameters'}</code>
                     </p>
-
                     <br />
                 </div>
             </div>
